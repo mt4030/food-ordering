fe44bac3da80b03620469b53bf78b7a60f5c9299
mt4030 <mohsen.t4030@gmail.com>
2025-09-25 15:54:51 +0330

    compressed the image size

--- src/componants/layout/MealsSummary-hero.jsx
+++ src/componants/layout/MealsSummary-hero.jsx
@@ -4,7 +4,7 @@ export default function Hero(){
 
     return(
         <div className='flex flex-col mb-7'>
-        <img src={Hero1} alt="img of a table full of food" className='h-30 sm:h-auto' />
+        <img src={Hero1} alt="img of a table full of food" className='h-30 sm:h-auto' loading="lazy" />
 <article className='bg-blue-500 items-center flex flex-col w-[70%] -mt-6 m-auto text-white p-6 shadow-xl/30'>
     <h1 className='text-3xl sm:text-4xl  mb-4 text-center'><span className='font-bold'>D</span>elicious food, <span className='font-bold '>D</span>eliver to you </h1>
     <p className='text-center text-white/90 mb-2 '><span className='font-bold'>F</span>reshly grilled chicken served with a mix of quinoa, roasted veggies,
